29b69c420dae864534a7915ea5358733265a4cf3
Oriol Burgaya <oriolbv@gmail.com>
2020-05-19 22:55:03 +0200

    Top-down movement implemented

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+
+    Vector2 movement;
+
+    void Start() 
+    {
+        rb = this.GetComponentInChildren<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate() 
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);    
+    }
+}
