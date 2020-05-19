af8f4aba5d77f82dab3d5bcf57f5447a3710f89d
Oriol Burgaya <oriolbv@gmail.com>
2020-05-19 23:15:46 +0200

    Idle animations added into project

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,18 +7,24 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     private Rigidbody2D rb;
+    private Animator animator;
 
     Vector2 movement;
 
     void Start() 
     {
         rb = this.GetComponentInChildren<Rigidbody2D>();
+        animator = this.GetComponentInChildren<Animator>();
     }
     
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("horizontal", movement.x);
+        animator.SetFloat("vertical", movement.y);
+        animator.SetFloat("speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate() 
