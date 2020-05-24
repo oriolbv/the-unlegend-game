54df269923ae05a20509e633105f147835a3e205
Oriol Burgaya <oriolbv@gmail.com>
2020-05-24 13:00:01 +0200

    BlueEnemy collision with sword added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,17 +4,38 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-
+    
+    [Header("Components")]
     private Rigidbody2D rb;
     private Animator animator;
 
+    [Header("Movement")]
     Vector2 movement;
+    public float moveSpeed = 5f;
+
+    [Header("Hit zones")]
+    private GameObject frontHitZone;
+    private GameObject rearHitZone;
+    private GameObject leftHitZone;
+    private GameObject rightHitZone;
+
 
     void Start() 
     {
         rb = this.GetComponentInChildren<Rigidbody2D>();
         animator = this.GetComponentInChildren<Animator>();
+
+        // Obtaining the different hit zones
+        frontHitZone = GameObject.Find("FrontHitZone");
+        rearHitZone = GameObject.Find("RearHitZone");
+        leftHitZone = GameObject.Find("LeftHitZone");
+        rightHitZone = GameObject.Find("RightHitZone");
+
+        // Set active flag to false for all hit zones
+        frontHitZone.SetActive(false);
+        rearHitZone.SetActive(false);
+        leftHitZone.SetActive(false);
+        rightHitZone.SetActive(false);
     }
     
     void Update()
@@ -41,9 +62,13 @@ public class PlayerMovement : MonoBehaviour
 
     private IEnumerator Attack()
     {
+        frontHitZone.SetActive(true);
         animator.SetBool("is_attacking", true);
         yield return null;
         animator.SetBool("is_attacking", false);
+        yield return new WaitForSeconds(.50f);
+        frontHitZone.SetActive(false);
+
     }
 
     void FixedUpdate() 
