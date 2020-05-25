6d66665c2a26682ed4f6524f5fbfc9c8ec776744
Oriol Burgaya <oriolbv@gmail.com>
2020-05-25 23:34:52 +0200

    Door triggering added

---
+++ Assets/Scripts/FloorButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorButton : MonoBehaviour
+{
+
+    public GameObject DoorGameObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.CompareTag("box"))
+        {
+            DoorGameObject.SetActive(false);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        DoorGameObject.SetActive(true);
+    }
+}
