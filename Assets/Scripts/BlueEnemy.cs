38a8735d50bd4e0165bd92ea5b684cacb6080977
Oriol Burgaya <oriolbv@gmail.com>
2020-05-26 18:55:48 +0200

    BlueEnemy movement added

--- Assets/Scripts/BlueEnemy.cs
+++ Assets/Scripts/BlueEnemy.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class BlueEnemy : MonoBehaviour
 {
+    [Header("Movement")]
+    private float speed = 2f;
+    private bool moveRight = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +17,24 @@ public class BlueEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (moveRight)
+        {
+            transform.Translate(2 * Time.deltaTime * speed, 0, 0);
+        }
+        else
+        {
+            transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+        }
         
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("wall"))
+        {
+            moveRight = !moveRight;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
