ad71a246e33b9ac5062135e151a993ac037d535b
Oriol Burgaya <oriolbv@gmail.com>
2020-05-30 12:50:24 +0200

    First approach of StartScene

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("DungeonScene");
+    }
+
+    public void GoToOptions()
+    {
+        SceneManager.LoadScene("OptionsScene");
+    }
+
+    public void GoToCredits()
+    {
+        SceneManager.LoadScene("CreditsScene");
+    }
+}
