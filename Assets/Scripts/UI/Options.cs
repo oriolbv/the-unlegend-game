8f3883ae2e375726ad6f75be3f8df65c7266ec0f
Oriol Burgaya <oriolbv@gmail.com>
2020-05-30 13:41:24 +0200

    Options menu first approach

--- Assets/Scripts/UI/Options.cs
+++ Assets/Scripts/UI/Options.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Options : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void GoBackToMenu()
     {
-        
+        SceneManager.LoadScene("MenuScene");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ApplyChanges()
     {
-        
+        // Store changed settings into OptionsSingleton
+
+        // Go back to Menu
+        SceneManager.LoadScene("MenuScene");
     }
+
+
 }
