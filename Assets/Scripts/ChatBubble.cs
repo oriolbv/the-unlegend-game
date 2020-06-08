88dcc9a9edc0b3fa3255ddffffc0b737670ebd82
Oriol Burgaya <oriolbv@gmail.com>
2020-06-08 20:00:28 +0200

    Second room sign added

--- Assets/Scripts/ChatBubble.cs
+++ Assets/Scripts/ChatBubble.cs
@@ -20,17 +20,17 @@ public class ChatBubble : ExtendedBehaviour
         textMeshPro.SetText(text);
         textMeshPro.ForceMeshUpdate();
         Vector2 textSize = textMeshPro.GetRenderedValues(false);
-        Vector2 padding = new Vector2(-2f, 0f);
+        Vector2 padding = new Vector2(-1f, 0f);
         backgroundSpriteRenderer.size = textSize + padding;
 
         Vector3 offset = new Vector3(-1f, 0f);
         backgroundSpriteRenderer.transform.localPosition = new Vector3(backgroundSpriteRenderer.size.x / 2f, 0f) + offset;
     }
 
-    public void Create(ChatBubble chatBubble, Transform parent, Vector3 localPosition, string text)
+    public void Create(ChatBubble chatBubble, Transform parent, Vector3 localPosition, string text, Vector3 positionOffset)
     {
         Transform chatBubbleTransform = Instantiate(chatBubble.transform, parent, true);
-        chatBubbleTransform.localPosition = localPosition + new Vector3(8f, -2f);
+        chatBubbleTransform.localPosition = localPosition + positionOffset;
         chatBubbleTransform.GetComponent<ChatBubble>().Setup(text);
 
         Wait(3f, () => {
--- Assets/Scripts/Sign.cs
+++ Assets/Scripts/Sign.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sign : ExtendedBehaviour
+public class Sign : MonoBehaviour
 {
     public ChatBubble ChatBubble;
     public string Text;
+    public Vector3 PositionOffset;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Player"))
         {
-            ChatBubble.Create(ChatBubble, this.transform, this.transform.localPosition, Text);
+            ChatBubble.Create(ChatBubble, this.transform, this.transform.localPosition, Text, PositionOffset);
         }
     }
 }
