398a07e3a081166730f35e73d42ea6f86a504c26
Oriol Burgaya <oriolbv@gmail.com>
2020-06-09 22:53:15 +0200

    Splash scene added

---
+++ Assets/Scripts/UI/Splash.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ToStartScene());
+    }
+
+    IEnumerator ToStartScene() 
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("StartScene");
+    }
+}
