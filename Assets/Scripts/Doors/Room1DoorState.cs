93cf6e344acccf4083af6b3f6569f7e750409c9b
Oriol Burgaya <oriolbv@gmail.com>
2020-06-10 07:23:22 +0200

    Room 1 door sound added

--- Assets/Scripts/Doors/Room1DoorState.cs
+++ Assets/Scripts/Doors/Room1DoorState.cs
@@ -2,11 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Room1DoorState : MonoBehaviour
+public class Room1DoorState : ExtendedBehaviour
 {
     public GameObject FloorButtonGameObject;
 
     private bool floorButtonPressed;
+    private bool doorOpened;
+
+    [Header("Sound Effects")]
+    private AudioSource audioSource;
+
+    public AudioClip DoorOpenAudioClip;
+
+    void Start()
+    {
+        doorOpened = false;
+
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = OptionsSingleton.Instance.EffectsLevel;
+        audioSource.clip = DoorOpenAudioClip;
+    }
 
     private void Update()
     {
@@ -14,10 +29,15 @@ public class Room1DoorState : MonoBehaviour
         floorButtonPressed = FloorButtonGameObject.GetComponent<FloorButton>().IsPressed;
 
         // Check if all floor buttons are pressed by the different boxes placed in the room
-        if (floorButtonPressed)
+        if (floorButtonPressed && !doorOpened)
         {
-            // Desactivate gameobject in order to open the door
-            this.gameObject.SetActive(false);
+            doorOpened = true;
+            audioSource.Play();
+            Wait(1.4f, () =>
+            {
+                // Desactivate gameobject in order to open the door
+                this.gameObject.SetActive(false);
+            });
         }
     }
 }
