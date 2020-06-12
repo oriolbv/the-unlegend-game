cea58f72f412ea32f2a0298e74299748cc058f11
Oriol Burgaya <oriolbv@gmail.com>
2020-06-12 07:43:15 +0200

    Some GameplayManager improvements and win sound effect added

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -6,6 +6,19 @@ using UnityEngine.SceneManagement;
 public class GameplayManager : MonoBehaviour
 {
 
+    [Header("Sound Effects")]
+    private AudioSource audioSource;
+
+    public AudioClip MainMusicAudioClip;
+    public AudioClip BossMusicAudioClip;
+
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = OptionsSingleton.Instance.MusicLevel;
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -13,8 +26,18 @@ public class GameplayManager : MonoBehaviour
         {
             Debug.Log("Game finished");
             // Reproduce WINNING song and go to credits scene.
-
-            
         }
+    }
+
+    public void ReproduceMainMusic()
+    {
+        audioSource.clip = MainMusicAudioClip;
+        audioSource.Play();
+    }
+
+    public void ReproduceBossMusic()
+    {
+        audioSource.clip = BossMusicAudioClip;
+        audioSource.Play();
     }
 }
